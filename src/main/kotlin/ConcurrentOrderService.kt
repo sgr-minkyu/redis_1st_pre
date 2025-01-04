00cd3d638ca4662dde9b99a45c344e5eca3a1662
MinKyu Song <sgr.kaus.australis@gmail.com>
2025-01-04 18:57:33 +0900

    [fix] ThreadPool에서 주문정보 테이블 재사용 되지 않도록 수정

--- src/main/kotlin/ConcurrentOrderService.kt
+++ src/main/kotlin/ConcurrentOrderService.kt
@@ -19,10 +19,12 @@ class ConcurrentOrderService {
                 val orderDatabase = threadLocalOrderDatabase.get()
                 val orders = orderDatabase.getOrPut(productName) { mutableListOf() }
                 orders.add(OrderInfo(productName, amount))
-                println("Thread ${Thread.currentThread().threadId()}의 주문 정보:")
-                println("\t${productName}: ${orders.size}건 ([${orders.last().amount}])")
                 threadLocalOrderDatabase.set(orderDatabase)
 
+                println("Thread ${Thread.currentThread().threadId()}의 주문 정보:")
+                println("\t${productName}: ${threadLocalOrderDatabase.get()[productName]!!.size}건 ([${threadLocalOrderDatabase.get()[productName]!!.last().amount}])")
+
+                threadLocalOrderDatabase.remove()
                 currentStock - amount
             } else {
                 currentStock
