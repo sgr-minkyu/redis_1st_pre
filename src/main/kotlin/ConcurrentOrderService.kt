10fce60e101438079984472a550bf78a25cffc94
MinKyu Song <sgr.kaus.australis@gmail.com>
2025-01-04 17:31:13 +0900

    [feat] 동시성 이슈 대응 서비스 추가

---
+++ src/main/kotlin/ConcurrentOrderService.kt
@@ -0,0 +1,37 @@
+import java.util.concurrent.ConcurrentHashMap
+
+class ConcurrentOrderService {
+    private val productDatabase = ConcurrentHashMap<String, Int>()
+
+    // 주문 정보를 저장하는 DB
+    private val threadLocalOrderDatabase = ThreadLocal.withInitial { mutableMapOf<String, MutableList<OrderInfo>>() }
+
+    init {
+        productDatabase.putAll(mapOf("apple" to 100, "banana" to 50, "orange" to 75))
+    }
+
+    // 주문 처리 메서드
+    fun order(productName: String, amount: Int) {
+        Thread.sleep(5) // 동시성 이슈 유발을 위한 인위적 지연 (해당 값을 조정하면서 테스트할 수 있습니다.)
+
+        productDatabase.computeIfPresent(productName) {_, currentStock ->
+            if(currentStock >= amount) {
+                val orderDatabase = threadLocalOrderDatabase.get()
+                val orders = orderDatabase.getOrPut(productName) { mutableListOf() }
+                orders.add(OrderInfo(productName, amount))
+                println("Thread ${Thread.currentThread().threadId()}의 주문 정보:")
+                println("\t${productName}: ${orders.size}건 ([${orders.last().amount}])")
+                threadLocalOrderDatabase.set(orderDatabase)
+
+                currentStock - amount
+            } else {
+                currentStock
+            }
+        }
+    }
+
+    // 재고 조회
+    fun getStock(productName: String): Int {
+        return productDatabase.getOrDefault(productName, 0)
+    }
+}
